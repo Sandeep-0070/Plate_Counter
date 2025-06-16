c10e80f7426136a224a9da5f1917cec6be6d2588
Siva Sandeep S <sandeepsivakanthan@gmail.com>
2025-06-16 11:09:10 +0530

    Added freeze1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
     formData.append("file", blob, "capture.jpg");
 
     try {
-      const res = await axios.post("http://127.0.0.1:5000/count-plates", formData);
+      const res = await axios.post("https://plate-counter-backend.onrender.com/count-plates", formData);
       setCount(res.data.count);
     } catch (err) {
       console.error("Error:", err);
